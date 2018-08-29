95c23ebe56745b0cb4ef470505ced3047f877f78
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-08-29 14:18:36 +0430

    export data to png

--- codes/6_degree_analysis.r
+++ codes/6_degree_analysis.r
@@ -13,27 +13,36 @@ indegree.histogram <- count(indegree)
 outdegree.histogram <- count(outdegree)
 
 # Indegree
+png(filename="in_degree_distribution.png", width=2048, height=2048)
 ggplot(indegree.histogram, aes(x = x, y = freq)) +
-  geom_point(size=1, colour="red", shape="square") +
-  scale_x_continuous("Indegree", trans = "log10") +
+  geom_point(size=2.5, colour="red", shape=20) +
+  scale_x_continuous("In-Degree", trans = "log10") +
   scale_y_continuous("No. Of Nodes", trans = "log10") +
-  ggtitle("InDegree Distribution")
+  ggtitle("In-Degree Distribution") +
+  theme(text = element_text(size=40))
+dev.off()
 
 # outdegree
+png(filename="out_degree_distribution.png", width=2048, height=2048)
 ggplot(outdegree.histogram, aes(x = x, y = freq)) +
-  geom_point(size=1, colour="red", shape="square") +
-  scale_x_continuous("Outdegree", trans = "log10") +
+  geom_point(size=2.5, colour="red", shape=20) +
+  scale_x_continuous("Out-Degree", trans = "log10") +
   scale_y_continuous("No. Of Nodes", trans = "log10") +
-  ggtitle("Outdegree Distribution")
-
+  ggtitle("Out-Degree Distribution") +
+  theme(text = element_text(size=40))
+dev.off()
 
 # Correlation
 cor <- cor(indegree, outdegree)
 cor
 
+png(filename="in_out_degree_correlation_distribution.png", width=2048, height=2048)
 corr.df <- data.frame(indegree, outdegree)
 ggplot(corr.df, aes(x = indegree, y = outdegree)) +
   geom_point(size=2, colour="red", shape=19) +
   scale_x_continuous("InDegree", trans = "log10") +
   scale_y_continuous("OutDegree", trans = "log10") +
-  geom_smooth(method=lm, se=FALSE)
+  geom_smooth(method=lm, se=FALSE) + 
+  ggtitle("In-Degree/Out-Degree Correlation") +
+  theme(text = element_text(size=40))
+dev.off()
