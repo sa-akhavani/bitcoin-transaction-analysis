25746572ccab2a74849706b07f6798f3e9fe4f8f
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-08-31 16:27:14 +0430

    Change sizing in hubs and authorities

--- codes/3_hubs_and_authorities.r
+++ codes/3_hubs_and_authorities.r
@@ -1,28 +1,42 @@
 library("igraph")
 
 # Creating Graph
-links <- read.csv("./5000_simplified_dataframe.data", header=T, as.is=T, sep=",")
-net <- graph_from_data_frame(d=links, directed=T)
-E(net)$arrow.mode <- 0
-E(net)$arrow.size <- .2
-E(net)$color <- "gray50"
+all_links <- read.csv("./au_graph.txt", header=T, as.is=T, sep="\t")
+# links <- all_links[sample(nrow(all_links), 7000),]
+links <- all_links
+links$weight = 1
 
-deg <- degree(net, mode="all")
-V(net)$color <- "pink"
-V(net)$degree <- degree(net, mode="all")
-V(net)$label <- ifelse(V(net)$degree<5,NA,V(net)$name)
-V(net)$label.font <- 2
-V(net)$label.color <- "red"
-V(net)$label.cex <- 0.5
-l <- layout_with_fr(net)
+net <- graph_from_data_frame(d=links, directed=T)
+net <- simplify(net, remove.multiple = T, remove.loops = T, edge.attr.comb="sum")
+
+
+V(net)$indegree <- degree(net, mode="in")
+V(net)$outdegree <- degree(net, mode="out")
 
 # Hubs are nodes with more outgoing
-hs <- hub_score(net, weights=NA)$vector
+hs <- hub_score(net)$vector
 # Authorities are nodes with more incomming
-as <- authority_score(net, weights=NA)$vector
+as <- authority_score(net)$vector
 
-png(filename="sample5000_hubs_authorities_layout_with_fr.png", width=1920, height=1080)
+# Visualization
+E(net)$arrow.mode <- 0
+E(net)$width <- sqrt(E(net)$weight)
+E(net)$color <- "gray50"
+l <- layout_with_fr(net)
+V(net)$label.font <- 2
+V(net)$label.color <- "black"
+V(net)$label.cex <- 0.5
+
+
+png(filename="sample5000_hubs_authorities_layout_with_fr.png", width=3840, height=2160)
 par(mfrow=c(1,2))
-plot(net, vertex.size=hs*50, layout=l, main="Hubs") # Outgoing
-plot(net, vertex.size=as*30, layout=l, main="Authorities") # Incomming
+V(net)$color <- ifelse(V(net)$indegree<=3,"gray50",ifelse(V(net)$indegree<20,"yellow", ifelse(V(net)$indegree>=20,"red", "gray50")))
+V(net)$size <- sqrt(V(net)$outdegree) * 3
+V(net)$label <- ifelse(V(net)$outdegree< 5 ,NA,V(net)$name)
+plot(net, vertex.label=NA, layout=l, main="Hubs") # Outgoing
+
+V(net)$color <- ifelse(V(net)$outdegree<=3,"gray50",ifelse(V(net)$outdegree<20,"yellow", ifelse(V(net)$outdegree>=20,"red", "gray50")))
+V(net)$size <- sqrt(V(net)$indegree) * 3
+V(net)$label <- ifelse(V(net)$indegree< 5 ,NA,V(net)$name)
+plot(net, vertex.label=NA, layout=l, main="Authorities", cex.lab=1.5, cex.axis=1.5, cex.main=1.5, cex.sub=1.5) # Incomming
 dev.off()
