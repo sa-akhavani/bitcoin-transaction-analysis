05bf6ce597c8a92f8820abf77b4e0a203840bdaf
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2018-08-31 14:25:36 +0430

    change community detection algorithm

--- codes/4_community.r
+++ codes/4_community.r
@@ -1,32 +1,32 @@
 library("igraph")
 
 # Creating Graph
-links <- read.csv("./5000_simplified_dataframe.data", header=T, as.is=T, sep=",")
-links <- links[sample(nrow(links), 150),]
-net <- graph_from_data_frame(d=links, directed=F)
+all_links <- read.csv("./au_graph.txt", header=T, as.is=T, sep="\t")
+links <- all_links[sample(nrow(all_links), 500),]
 
-# clusters <- cluster_fast_greedy(net)
-clusters <- cluster_label_prop(net)
+net <- graph_from_data_frame(d=links, directed=F)
+net <- simplify(net, remove.loops = T)
+
+# Removing Vertices with degree==1
+V(net)$degree = degree(net, mode="all")
+net <- delete_vertices(net, V(net)[degree<1])
+
+
+clusters <- cluster_edge_betweenness(net)
 
 ## Plot Community
 E(net)$arrow.mode <- 0
 E(net)$color <- "orange"
 V(net)$size <- degree(net, mode="all")
-E(net)$width <- sqrt(E(net)$weight)
+E(net)$width <- 2
 V(net)$label <- ifelse(V(net)$size<0,NA,V(net)$name)
 V(net)$label.font <- 2
 V(net)$label.color <-"black"
 V(net)$label.cex <- 3
 
-# graph_attr(net, "layout") <- layout_with_fr(net)
-l <- layout_with_fr(net)
-# l <- layout_randomly
+# l <- layout_with_fr(net)
+l <- layout_randomly
 
 png(filename="community_graph.png", width=4096, height=4096)
 plot(clusters, net, layuot=l)
 dev.off()
-
-# Another Coloring
-# V(net)$community <- clusters$membership
-# colrs <- adjustcolor( c("gray50", "tomato", "gold", "yellowgreen"), alpha=.6)
-# plot(net, vertex.color=colrs[V(net)$community])
